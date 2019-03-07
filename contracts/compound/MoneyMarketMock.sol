144048c9f81d13c0dfb6ad78a50788a64c1288b6
Brendan Asselstine <mail.asselstine@gmail.com>
2019-03-07 14:30:47 -0800

    Made MoneyMarket functions external

--- contracts/compound/MoneyMarketMock.sol
+++ contracts/compound/MoneyMarketMock.sol
@@ -12,16 +12,16 @@ contract MoneyMarketMock is IMoneyMarket {
     token = IERC20(_token);
   }
 
-  function supply(address asset, uint amount) public returns (uint) {
+  function supply(address asset, uint amount) external returns (uint) {
     ownerTokenAmounts[msg.sender][asset] = amount;
     require(token.transferFrom(msg.sender, address(this), amount), "could not transfer tokens");
   }
 
-  function withdraw(address asset, uint requestedAmount) public returns (uint) {
+  function withdraw(address asset, uint requestedAmount) external returns (uint) {
     require(token.transfer(msg.sender, requestedAmount), "could not transfer tokens");
   }
 
-  function getSupplyBalance(address account, address asset) view public returns (uint) {
+  function getSupplyBalance(address account, address asset) view external returns (uint) {
     return (ownerTokenAmounts[account][asset] * 120) / 100;
   }
 }
