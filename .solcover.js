25d783da11bd196d2a3e4fc93b3c1d6d92a48c70
Brendan Asselstine <mail.asselstine@gmail.com>
2019-10-30 17:08:12 -0700

    Installed code coverage

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,10 @@
 module.exports = {
-  testCommand: 'yarn test --network coverage'
-}
+  mocha: { reporter: 'spec' },
+  skipFiles: [
+    "compound/ICErc20.sol",
+    "test/CErc20Mock.sol",
+    "test/ExposedDrawManager.sol",
+    "test/ExposedUniformRandomNumber.sol",
+    "test/Token.sol"
+  ]
+};
--- truffle-config.js
+++ truffle-config.js
@@ -12,14 +12,6 @@ module.exports = {
       network_id: '*'
     },
 
-    coverage: {
-      host: "localhost",
-      network_id: "*",
-      port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
-      gas: 0xfffffffffff, // <-- Use this high gas value
-      gasPrice: 0x01      // <-- Use this low gas price
-    },
-
     rinkeby: {
       provider: () => new HDWalletProvider(
         process.env.HDWALLET_MNEMONIC,
@@ -47,16 +39,11 @@ module.exports = {
     }
   },
 
+  plugins: ["solidity-coverage"],
+
   compilers: {
     solc: {
       version: "0.5.10",
-    }
-  },
-
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 100
     }
   },
 
