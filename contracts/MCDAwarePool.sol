66b78dcb41ffa106296055d9a768449d9baa0eb0
Brendan Asselstine <mail.asselstine@gmail.com>
2019-11-18 13:58:38 -0800

    Some changes from Quantstamp audit
    - Renamed BasePool#commit() to BasePool#emitCommitted()
    - Fixed a bug in DrawManager where deposits were not getting reset
    - Added docs to DrawManager
    - Started Documenting MCDAwarePool

--- contracts/MCDAwarePool.sol
+++ contracts/MCDAwarePool.sol
@@ -4,13 +4,31 @@ import "./ERC777Pool.sol";
 import "scd-mcd-migration/src/ScdMcdMigration.sol";
 import { GemLike } from "scd-mcd-migration/src/Interfaces.sol";
 
+/**
+ * @title MCDAwarePool
+ * @author Brendan Asselstine brendan@pooltogether.us
+ * @notice This contract is a Pool that is aware of the new Multi-Collateral Dai.  It uses the ERC777Recipient interface to
+ * detect if it's being transferred tickets from the old single collateral Dai (Sai) Pool.  If it is, it migrates the Sai to Dai
+ * and immediately deposits the new Dai as committed tickets for that user.  We are knowingly bypassing the committed period for
+ * users to encourage them to migrate to the MCD Pool.
+ */
 contract MCDAwarePool is ERC777Pool, IERC777Recipient {
 
-  // keccak("PoolTogether.MCDAwarePool")
+  /**
+   * @notice The ERC1820 interface hash that this pool implements.
+   *
+   * keccak("PoolTogether.MCDAwarePool")
+   */
   bytes32 public constant MCD_AWARE_POOL_INTERFACE_HASH = 0xf07efa750d04abfb9556d73b16e6ffb37436eb789c2a8fd17117e0bf232a506c;
 
+  /**
+   * @notice Returns the address of the ScdMcdMigration contract (see https://github.com/makerdao/developerguides/blob/master/mcd/upgrading-to-multi-collateral-dai/upgrading-to-multi-collateral-dai.md#direct-integration-with-smart-contracts)
+   */
   function scdMcdMigration() public view returns (ScdMcdMigration);
 
+  /**
+   * @notice Initializes the contract.
+   */
   function init (
     address _owner,
     address _cToken,
