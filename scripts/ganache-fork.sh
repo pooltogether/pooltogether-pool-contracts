f077429f6c66d246e02ae8f70e7760985762a81f
Brendan Asselstine <mail.asselstine@gmail.com>
2019-12-03 10:07:36 -0800

    Changes:
    - Added some additional tests for ERC777Pool
    - Downgraded Ganache CLI
    - Tweaked MCDUpgrade test code

---
+++ scripts/ganache-fork.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+mkdir -p .ganache
+ganache-cli \
+  -p 8546 \
+  -f "$GANACHE_FORK_URL" \
+  -u 0xbe0eb53f46cd790cd13851d5eff43d12404d33e8 \
+  -u 0x3a9f7c8ca36c42d7035e87c3304ee5cbd353a532 \
+  -u 0x0fda4ac09a12c10fae30e429f4d6b47c9a83c87e \
+  -u 0x2c6e8512d1cd7fc226ed359269c39a243bd0bb67
--- upgrade-test/MCDAwarePoolUpgrade.test.js
+++ upgrade-test/MCDAwarePoolUpgrade.test.js
@@ -25,39 +25,44 @@ async function test (context) {
   let withdrawalsCount = 1
   let userBalances = {}
 
+  let address = '0x2c6e8512d1cd7fc226ed359269c39a243bd0bb67'
+  let signer = provider.getSigner(address)
+  let pool = new ethers.Contract(contracts.Pool.address, artifacts.Pool.abi, signer)
+
   // Now ensure we can withdraw the top 5
-  for (let i = 0; i < withdrawalsCount; i++) {
-    let address = users[i].id
+  // for (let i = 0; i < withdrawalsCount; i++) {
+    // let address = users[i].id
     // Make sure they have enough ether
+    console.log(chalk.green(`sending eth from ${BINANCE_ADDRESS} to ${address}...`))
     let binTx2 = await binance.sendTransaction({ to: address, value: ethers.utils.parseEther('1') })
     await provider.waitForTransaction(binTx2.hash)
-    let signer = provider.getSigner(address)
-    let pool = new ethers.Contract(contracts.Pool.address, artifacts.Pool.abi, signer)
+    // let signer = provider.getSigner(address)
+    // let pool = new ethers.Contract(contracts.Pool.address, artifacts.Pool.abi, signer)
     userBalances[address] = await pool.balanceOf(address)
-    let balance = ethers.utils.formatEther(userBalances[address])
-    console.log(chalk.green(`Withdrawal ${i}: ${address} is withdrawing ${balance}`))
+    // let balance = ethers.utils.formatEther(userBalances[address])
+    // console.log(chalk.green(`Withdrawal ${i}: ${address} is withdrawing ${balance}`))
     let tx = await pool.withdraw()
     await provider.waitForTransaction(tx.hash)
-  }
+  // }
 
   // now deposit again
-  for (let i = 0; i < withdrawalsCount; i++) {
-    let address = users[i].id
+  // for (let i = 0; i < withdrawalsCount; i++) {
+    // let address = users[i].id
     // Make sure they have enough ether
-    let signer = provider.getSigner(address)
-    let pool = new ethers.Contract(contracts.Pool.address, artifacts.Pool.abi, signer)
+    // let signer = provider.getSigner(address)
+    // let pool = new ethers.Contract(contracts.Pool.address, artifacts.Pool.abi, signer)
     let token = new ethers.Contract(await pool.token(), artifacts.ERC20.abi, signer)
-    let balance = userBalances[address]
-    console.log(chalk.green(`Depositing ${i}: ${address} is depositing ${balance}`))
+    let depositBalance = userBalances[address]
+    console.log(chalk.green(`Depositing ${i}: ${address} is depositing ${depositBalance}`))
 
     console.log(`user balance: ${ethers.utils.formatEther(await token.balanceOf(address))}`)
 
     // let tx = await token.approve(pool.address, balance)
     // await provider.waitForTransaction(tx.hash)
 
-    tx = await pool.depositPool(balance)
+    tx = await pool.depositPool(depositBalance)
     await provider.waitForTransaction(tx.hash)
-  }
+  // }
 }
 
 async function run() {
