155b5a37b231e03e9002faa78d1dfb30467a19de
Brendan Asselstine <mail.asselstine@gmail.com>
2019-12-04 10:57:45 -0800

    Improved fork tests

--- upgrade-test/forkMainnet.js
+++ upgrade-test/forkMainnet.js
@@ -3,7 +3,8 @@ const ganache = require("ganache-cli");
 module.exports = function ({ unlockedAccounts }) {
   const server = ganache.server({
     fork: process.env.GANACHE_FORK_URL,
-    unlocked_accounts: unlockedAccounts
+    unlocked_accounts: unlockedAccounts,
+    hardfork: 'constantinople'
   });
   return new Promise((resolve, reject) => {
     server.listen('8546', function(err, blockchain) {
