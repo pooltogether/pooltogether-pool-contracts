88a599519c5c7b66ff089da6bba87df044afef80
Brendan Asselstine <mail.asselstine@gmail.com>
2019-12-16 15:22:55 -0800

    Updated fork setup

--- scripts/fork-setup.sh
+++ scripts/fork-setup.sh
@@ -4,6 +4,7 @@ oz compile
 yarn fork pay
 yarn fork push
 yarn fork upgrade-v2x
+# for some reason deploy dai breaks the sai contract
 yarn fork deploy-dai
-# yarn fork reward dai 2
-# yarn fork migrate-sai
+# yarn fork reward-open dai 3
+# yarn fork reward-open sai 2
