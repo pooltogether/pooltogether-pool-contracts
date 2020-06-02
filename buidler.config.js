22100d2a62a2973d5cc1e3dcc35ac191c562e431
Brendan Asselstine <mail.asselstine@gmail.com>
2020-06-02 10:48:59 -0700

    Simplified the buidler config

--- buidler.config.js
+++ buidler.config.js
@@ -23,14 +23,6 @@ module.exports = {
   paths: {
     artifacts: "./build"
   },
-  etherscan: {
-    // The url for the Etherscan API you want to use.
-    // For example, here we're using the one for the Ropsten test network
-    url: process.env.ETHERSCAN_API_URL,
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: process.env.ETHERSCAN_API_KEY
-  },
   networks: {
     buidlerevm: {
       blockGasLimit: 200000000,
@@ -42,17 +34,10 @@ module.exports = {
       gas: 200000000
     },
     local: {
-      url: 'http://127.0.0.1:' + process.env.LOCAL_BUIDLEREVM_PORT,
+      url: 'http://127.0.0.1:' + process.env.LOCAL_BUIDLEREVM_PORT || '8545',
       gas: 200000000,
       blockGasLimit: 200000000
-    },
-    kovan: {
-      accounts: {
-        mnemonic: process.env.HDWALLET_MNEMONIC
-      },
-      url: process.env.INFURA_PROVIDER_URL_KOVAN,
-      chainId: 42
-    },
+    }
   },
   gasReporter: {
     currency: 'CHF',
