9be38f8556d897f18491e07d697840f4dfca5802
Rob Secord <rob@pooltogether.com>
2020-06-16 14:58:55 -0400

    Refactors for Sponsorship, Updates for unit-tests

--- test/Sponsorship.test.js
+++ test/Sponsorship.test.js
@@ -1,6 +1,6 @@
 const { deployContract, deployMockContract } = require('ethereum-waffle')
 const { deploy1820 } = require('deploy-eip-1820')
-const Sponsorship = require('../build/SponsorshipHarness.json')
+const SponsorshipHarness = require('../build/SponsorshipHarness.json')
 const PeriodicPrizePool = require('../build/PeriodicPrizePool.json')
 const Credit = require('../build/Credit.json')
 const Timelock = require('../build/Timelock.json')
@@ -17,6 +17,7 @@ const { call } = require('./helpers/call')
 const { ethers } = require('./helpers/ethers')
 const { expect } = require('chai')
 const buidler = require('./helpers/buidler')
+const getIterable = require('./helpers/iterable')
 const { CALL_EXCEPTION } = require('ethers/errors')
 
 const toWei = ethers.utils.parseEther
@@ -38,6 +39,33 @@ describe.only('Sponsorship contract', function() {
   let prizePool, yieldService, manager, token, interestTracker, sponsorshipCredit
 
   let lastTxTimestamp
+
+  const _mocksForSupply = async ({supply, mintedTickets, collateral, account = wallet}) => {
+    await token.mock.transferFrom.withArgs(account._address, sponsorship.address, supply).returns(true)
+      
+    // ensure yield service approved
+    await token.mock.allowance.returns(0)
+    await token.mock.approve.returns(true)
+    
+    // supply to yield service
+    await yieldService.mock.supply.withArgs(supply).returns()
+    await prizePool.mock.mintedTickets.withArgs(mintedTickets).returns()
+    await interestTracker.mock.supplyCollateral.withArgs(collateral).returns(collateral)
+  }
+
+  const _mocksForRedeem = async ({redeem, redeemedTickets, transfer, account = wallet}) => {
+    await prizePool.mock.redeemedTickets.withArgs(redeemedTickets).returns()
+    await yieldService.mock.redeem.withArgs(redeem).returns()
+    await token.mock.transfer.withArgs(account._address, transfer).returns(true)
+  }
+
+  const _mocksForSweep = async ({totalSupply, redeemCollateral, credit, account = wallet, exchangeRate = toWei('1')}) => {
+      await interestTracker.mock.totalSupply.returns(totalSupply)
+      await interestTracker.mock.redeemCollateral.withArgs(redeemCollateral).returns(redeemCollateral)
+      await interestTracker.mock.exchangeRateMantissa.returns(exchangeRate);
+      await sponsorshipCredit.mock.mint.withArgs(account._address, credit).returns()
+  }
+
 
   beforeEach(async () => {
     [wallet, wallet2] = await buidler.ethers.getSigners()
@@ -66,7 +94,7 @@ describe.only('Sponsorship contract', function() {
     await manager.mock.timelock.returns(timelock.address)
     await manager.mock.sponsorshipCredit.returns(sponsorshipCredit.address)
 
-    sponsorship = await deployContract(wallet, Sponsorship, [], overrides)
+    sponsorship = await deployContract(wallet, SponsorshipHarness, [], overrides)
 
     let tx = await sponsorship['initialize(address,address,string,string)'](
       manager.address,
@@ -88,103 +116,106 @@ describe.only('Sponsorship contract', function() {
 
   describe('supply()', () => {
     it('should mint sponsorship tokens', async () => {
-      let amount = toWei('10')
+      const sweepAmount = toWei('0')
+      const supplyAmount = toWei('10')
 
-      await token.mock.transferFrom.withArgs(wallet._address, sponsorship.address, amount).returns(true)
-      
-      // ensure yield service approved
-      await token.mock.allowance.returns(0)
-      await token.mock.approve.returns(true)
-      
-      // supply to yield service
-      await yieldService.mock.supply.withArgs(amount).returns()
-      await prizePool.mock.mintedTickets.withArgs(toWei('10')).returns()
-      await interestTracker.mock.supplyCollateral.withArgs(amount).returns(amount)
+      await _mocksForSupply({
+        supply: supplyAmount, 
+        mintedTickets: supplyAmount, 
+        collateral: supplyAmount,
+      })
 
-      await sponsorship.supply(wallet._address, toWei('10'), [])
+      await _mocksForSweep({
+        totalSupply: sweepAmount, 
+        redeemCollateral: sweepAmount, 
+        credit: sweepAmount,
+      })
 
-      expect(await sponsorship.balanceOfInterestShares(wallet._address)).to.equal(amount)
-      expect(await sponsorship.balanceOf(wallet._address)).to.equal(amount)
+      // Supply sponsorship
+      await sponsorship.supply(wallet._address, supplyAmount)
+
+      // Test supply
+      expect(await sponsorship.balanceOfInterestShares(wallet._address)).to.equal(supplyAmount)
+      expect(await sponsorship.balanceOf(wallet._address)).to.equal(supplyAmount)
     })
   })
 
   describe('redeem()', () => {
     it('should allow a sponsor to redeem their sponsorship tokens', async () => {
-      await sponsorship.setInterestSharesForTest(wallet._address, toWei('10'))
-      await sponsorship.mintForTest(wallet._address, toWei('10'))
+      const amount = toWei('10')
 
-      // burn tickets
-      await prizePool.mock.redeemedTickets.withArgs(toWei('10')).returns()
+      // Pre-fund sponsorship tokens
+      await sponsorship.setInterestSharesForTest(wallet._address, amount)
+      await sponsorship.mintForTest(wallet._address, amount)
 
-      // credit user
-      await interestTracker.mock.totalSupply.returns(toWei('10'))
-      await interestTracker.mock.redeemCollateral.withArgs(toWei('10')).returns(toWei('10'))
-      await interestTracker.mock.exchangeRateMantissa.returns(toWei('1'));
-      await sponsorshipCredit.mock.mint.withArgs(wallet._address, toWei('10')).returns()
+      await _mocksForSweep({
+        totalSupply: amount, 
+        redeemCollateral: amount, 
+        credit: toWei('0'),
+      })
 
-      await yieldService.mock.redeem.withArgs(toWei('10')).returns()
+      await _mocksForRedeem({
+        redeem: amount, 
+        redeemedTickets: amount, 
+        transfer: amount,
+      })
 
-      await token.mock.transfer.withArgs(wallet._address, toWei('10')).returns(true)
-
-      await expect(sponsorship.redeem(toWei('10'), []))
+      // Test redeem
+      await expect(sponsorship.redeem(amount))
         .to.emit(sponsorship, 'SponsorshipRedeemed')
-        .withArgs(wallet._address, wallet._address, toWei('10'))
+        .withArgs(wallet._address, wallet._address, amount)
     })
 
     it('should not allow a sponsor to redeem more sponsorship tokens than they hold', async () => {
-      await sponsorship.setInterestSharesForTest(wallet._address, toWei('10'))
-      await sponsorship.mintForTest(wallet._address, toWei('10'))
+      const amount = toWei('10')
 
-      // burn tickets
-      await prizePool.mock.redeemedTickets.withArgs(toWei('10')).returns()
+      // Pre-fund sponsorship tokens
+      await sponsorship.setInterestSharesForTest(wallet._address, amount)
 
-      // credit user
-      await interestTracker.mock.totalSupply.returns(toWei('10'))
-      await interestTracker.mock.redeemCollateral.withArgs(toWei('10')).returns(toWei('10'))
-      await interestTracker.mock.exchangeRateMantissa.returns(toWei('1'));
-      await sponsorshipCredit.mock.mint.withArgs(wallet._address, toWei('10')).returns()
-
-      await yieldService.mock.redeem.withArgs(toWei('10')).returns()
-
-      await token.mock.transfer.withArgs(wallet._address, toWei('10')).returns(true)
-
-      await expect(sponsorship.redeem(toWei('10'), []))
-        .to.emit(sponsorship, 'SponsorshipRedeemed')
-        .withArgs(wallet._address, wallet._address, toWei('10'))
+      // Test balance revert
+      await expect(sponsorship.redeem(amount.mul(2)))
+        .to.be.revertedWith('Sponsorship/insuff')
     })
   })
 
   describe('operatorRedeem()', () => {
     it('should allow an operator to redeem on behalf of a sponsor their sponsorship tokens', async () => {
-      await sponsorship.setInterestSharesForTest(wallet._address, toWei('10'))
-      await sponsorship.mintForTest(wallet._address, toWei('10'))
+      const amount = toWei('10')
+
+      // Pre-fund sponsorship tokens
+      await sponsorship.setInterestSharesForTest(wallet._address, amount)
+      await sponsorship.mintForTest(wallet._address, amount)
+
+      await _mocksForSweep({
+        totalSupply: amount, 
+        redeemCollateral: amount, 
+        credit: toWei('0'),
+      })
+
+      await _mocksForRedeem({
+        redeem: amount, 
+        redeemedTickets: amount, 
+        transfer: amount,
+      })
 
       // approve operator
       await sponsorship.authorizeOperator(wallet2._address)
 
-      // burn tickets
-      await prizePool.mock.redeemedTickets.withArgs(toWei('10')).returns()
-
-      // credit user
-      await interestTracker.mock.totalSupply.returns(toWei('10'))
-      await interestTracker.mock.redeemCollateral.withArgs(toWei('10')).returns(toWei('10'))
-      await interestTracker.mock.exchangeRateMantissa.returns(toWei('1'));
-      await sponsorshipCredit.mock.mint.withArgs(wallet._address, toWei('10')).returns()
-
-      await yieldService.mock.redeem.withArgs(toWei('10')).returns()
-
-      await token.mock.transfer.withArgs(wallet._address, toWei('10')).returns(true)
-
-      await expect(sponsorship.connect(wallet2).operatorRedeem(wallet._address, toWei('10'), []))
+      // Test operator redeem
+      await expect(sponsorship.connect(wallet2).operatorRedeem(wallet._address, amount))
         .to.emit(sponsorship, 'SponsorshipRedeemed')
-        .withArgs(wallet2._address, wallet._address, toWei('10'))
+        .withArgs(wallet2._address, wallet._address, amount)
     })
 
     it('should not allow an unapproved operator to redeem on behalf of a sponsor', async () => {
-      await sponsorship.setInterestSharesForTest(wallet._address, toWei('10'))
-      await sponsorship.mintForTest(wallet._address, toWei('10'))
+      const amount = toWei('10')
 
-      await expect(sponsorship.connect(wallet2).operatorRedeem(wallet._address, toWei('10'), []))
+      // Pre-fund sponsorship tokens
+      await sponsorship.setInterestSharesForTest(wallet._address, amount)
+      await sponsorship.mintForTest(wallet._address, amount)
+
+      // Test redeem revert
+      await expect(sponsorship.connect(wallet2).operatorRedeem(wallet._address, amount))
         .to.be.revertedWith('TokenModule/Invalid operator');
     })
   })
@@ -200,7 +231,53 @@ describe.only('Sponsorship contract', function() {
   })
 
   describe('sweep()', () => {
-    it('should allow anyone to sweep for a list of users')
+    it('should allow anyone to sweep for a list of users', async () => {
+      const numAccounts = 5
+      const iterableAccounts = getIterable(await buidler.ethers.getSigners(), numAccounts)
+      const amounts = [toWei('10'), toWei('98765'), toWei('100'), toWei('100000000'), toWei('10101101')]
+      const accountAddresses = []
+      const interestAmount = toWei('1')
+      let totalSupply = toWei('0')
+
+      // Pre-fund sponsorship tokens *with interest*
+      for await (let user of iterableAccounts()) {
+        await sponsorship.mintForTest(user.data._address, amounts[user.index])
+        await sponsorship.setInterestSharesForTest(user.data._address, amounts[user.index].add(interestAmount))
+
+        accountAddresses.push(user.data._address)
+        totalSupply = totalSupply.add(amounts[user.index])
+      }
+      debug({accountAddresses})
+
+      // Mocks for multiple accounts
+      for await (let user of iterableAccounts()) {
+        await _mocksForSweep({
+          account: user.data,
+          totalSupply: totalSupply, 
+          redeemCollateral: interestAmount,
+          credit: interestAmount,
+        })
+
+        await _mocksForRedeem({
+          account: user.data,
+          redeem: amounts[user.index], 
+          redeemedTickets: amounts[user.index], 
+          transfer: amounts[user.index],
+        })
+        totalSupply = totalSupply.sub(amounts[user.index])
+      }
+
+      // Sweep for multiple accounts
+      await expect(sponsorship.sweep(accountAddresses))
+        .to.emit(sponsorship, 'SponsorshipSwept')
+        .withArgs(wallet._address, accountAddresses)
+
+      // Test balances; all interest swept
+      for await (let user of iterableAccounts()) {
+        expect(await sponsorship.balanceOfInterestShares(user.data._address)).to.equal(amounts[user.index]) // "interestAmount" swept
+        expect(await sponsorship.balanceOf(user.data._address)).to.equal(amounts[user.index])
+      }
+    })
   })
 
 });
