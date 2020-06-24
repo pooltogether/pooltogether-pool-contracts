adc5957d084fdb66d84a8d4579fc07a84fc866d1
Rob Secord <rob@pooltogether.com>
2020-06-23 21:23:02 -0400

    Update unit-tests to cover locked vs unlocked tokens

--- test/Timelock.test.js
+++ test/Timelock.test.js
@@ -10,6 +10,7 @@ const getIterable = require('./helpers/iterable')
 const toWei = ethers.utils.parseEther
 const toBytes = ethers.utils.toUtf8Bytes
 const EMPTY_STR = toBytes('')
+const now = () => Math.floor((new Date()).getTime() / 1000)
 
 const debug = require('debug')('ptv3:Timelock.test')
 
@@ -52,11 +53,11 @@ describe('Timelock contract', function() {
   })
 
   describe('sweep()', () => {
-    it('should sweep & burn timelock tokens from the user', async () => {
+    it('should sweep unlocked tokens from the user', async () => {
       const amounts = [toWei('10'), toWei('98765'), toWei('100'), toWei('100000000'), toWei('10101101')]
       const iterableAccounts = getIterable(await buidler.ethers.getSigners(), amounts.length)
       const accountAddresses = []
-      const timestamp = 10000
+      const timestamp = now()
       let totalSupply = toWei('0')
 
       for await (let user of iterableAccounts()) {
@@ -80,13 +81,40 @@ describe('Timelock contract', function() {
 
       // Confirm balances
       for await (let user of iterableAccounts()) {
-        expect(await timelock.timelockBalanceOf(user.data._address)).to.equal(toWei('0'))
+        expect(await timelock.timelockBalanceOf(user.data._address)).to.equal(toWei('0')) // swept
         expect(await timelock.timelockBalanceAvailableAt(user.data._address)).to.equal(toWei('0'))
+      }
+    })
+
+    it('should not sweep locked tokens from the user', async () => {
+      const amounts = [toWei('10'), toWei('98765'), toWei('100'), toWei('100000000'), toWei('10101101')]
+      const iterableAccounts = getIterable(await buidler.ethers.getSigners(), amounts.length)
+      const accountAddresses = []
+      const timestamp = now() + (60 * 1000)
+      let totalSupply = toWei('0')
+
+      for await (let user of iterableAccounts()) {
+        await token.mock.transfer.withArgs(user.data._address, amounts[user.index]).returns(true)
+
+        await timelock.mintTo(user.data._address, amounts[user.index], timestamp)
+
+        accountAddresses.push(user.data._address)
+        totalSupply = totalSupply.add(amounts[user.index])
+      }
+
+      await timelock.setTokenBalanceForTest(totalSupply)
+
+      // Confirm Total Supply
+      expect(await timelock.totalSupply()).to.equal(totalSupply)
+
+      // Test sweep
+      await timelock.sweep(accountAddresses)
+
+      // Confirm balances
+      for await (let user of iterableAccounts()) {
+        expect(await timelock.timelockBalanceOf(user.data._address)).to.equal(amounts[user.index]) // not swept
+        expect(await timelock.timelockBalanceAvailableAt(user.data._address)).to.equal(timestamp)
       }
     })
   })
 });
-
-
-
-
