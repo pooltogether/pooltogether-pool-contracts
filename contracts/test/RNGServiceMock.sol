221a8113847e611de204874656b2b5f18d0c28d3
Rob Secord <rob@pooltogether.com>
2020-07-16 23:19:37 -0400

    Update to use published package for RNG

--- contracts/test/RNGServiceMock.sol
+++ contracts/test/RNGServiceMock.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.6.4;
 
-import "../rng/RNGInterface.sol";
+import "@pooltogether/pooltogether-rng-contracts/contracts/RNGInterface.sol";
 
 contract RNGServiceMock is RNGInterface {
 
