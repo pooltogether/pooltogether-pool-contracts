2168191a2922eb692b913335510c29ed2cf26d84
Brendan Asselstine <mail.asselstine@gmail.com>
2020-08-13 14:41:20 -0700

    Collapsed Referrals and Standard Volume Drips (#99)
    - Comptroller now simply has a volume drip list
    - volume drip events are now sane

--- contracts/comptroller/ComptrollerStorage.sol
+++ contracts/comptroller/ComptrollerStorage.sol
@@ -3,12 +3,20 @@ pragma solidity ^0.6.4;
 import "@openzeppelin/contracts-ethereum-package/contracts/access/Ownable.sol";
 
 import "../drip/BalanceDripManager.sol";
-import "../drip/VolumeDripManager.sol";
+import "../drip/VolumeDrip.sol";
 
 contract ComptrollerStorage is OwnableUpgradeSafe {
   uint256 internal _reserveRateMantissa;
 
-  mapping(address => VolumeDripManager.State) internal referralVolumeDrips;
-  mapping(address => VolumeDripManager.State) internal volumeDrips;
+  uint256 lastVolumeDripId;
+  mapping(uint256 => VolumeDrip.State) volumeDrips;
+  mapping(uint256 => address) volumeDripTokens;
+
+  struct PrizeStrategyVolumeDripManager {
+    mapping(address => uint256[]) activeMeasureVolumeDripIndices;
+    mapping(address => uint256[]) activeMeasureReferralVolumeDripIndices;
+  }
+
+  mapping(address => PrizeStrategyVolumeDripManager) internal prizeStrategyVolumeDripManagers;
   mapping(address => BalanceDripManager.State) internal balanceDrips;
 }
--- test/features/referralVolumeDrip.test.js
+++ test/features/referralVolumeDrip.test.js
@@ -13,9 +13,9 @@ describe('Referral volume drip', () => {
 
   it('should drip users governance tokens', async () => {
     await env.createPool({ prizePeriodSeconds: 10, exitFee: '0.1', creditRate: '0.01' })
-    await env.referralVolumeDripGovernanceToken({ dripAmount: '100', periodSeconds: 10, startTime: 20 })
-    await env.buyTicketsAtTime({ user: 1, tickets: '10', elapsed: 30, referrer: 2 })
-    await env.claimReferralVolumeDripGovernanceTokensAtTime({ user: 2, elapsed: 40 })
+    await env.volumeDripGovernanceToken({ dripAmount: '100', periodSeconds: 10, startTime: 20, isReferral: true })
+    await env.buyTicketsAtTime({ user: 1, tickets: '10', referrer: 2, elapsed: 30 })
+    await env.claimVolumeDripAtTime({ user: 2, index: 1, elapsed: 40 })
     await env.expectUserToHaveGovernanceTokens({ user: 2, tokens: '100' })
   })
 })
