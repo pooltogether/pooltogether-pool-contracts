a5b2fe6e8ec5b776fc0e0f0938221785a2031dd3
Rob Secord <rob@pooltogether.com>
2020-09-24 16:31:59 -0400

    Add tests for deactivated referral volume drips

--- test/features/referralVolumeDrip.test.js
+++ test/features/referralVolumeDrip.test.js
@@ -35,4 +35,23 @@ describe('Referral volume drip', () => {
     await env.expectUserToHaveGovernanceTokens({ user: 3, tokens: '25' })
     await env.expectUserToHaveGovernanceTokens({ user: 4, tokens: '75' })
   })
+
+  it('should deactivate the drip when all tokens have been distributed', async () => {
+    await env.createPool({ prizePeriodStart: 0, prizePeriodSeconds: 10, creditLimit: '0.1', creditRate: '0.01' })
+    await env.volumeDripGovernanceToken({ dripAmount: '100', periodSeconds: 10, endTime: 40, isReferral: true })
+    // reduce drip-token balance to 100
+    await env.burnGovernanceTokensFromComptroller({ amount: '9900' })
+    // user 3 will get all of the available referral drip tokens
+    await env.buyTickets({ user: 1, tickets: '100', referrer: 3 })
+    await env.setCurrentTime(40)
+    await env.claimGovernanceDripTokens({ user: 3 })
+    await env.expectUserToHaveGovernanceTokens({ user: 3, tokens: '100' })
+    // user 4 will not receive any referral drip tokens after deactivation
+    await env.buyTickets({ user: 2, tickets: '100', referrer: 4 })
+    await env.setCurrentTime(80)
+    await env.claimGovernanceDripTokens({ user: 4 })
+    await env.expectUserToHaveGovernanceTokens({ user: 4, tokens: '0' })
+    // user 3 will still have only 100
+    await env.expectUserToHaveGovernanceTokens({ user: 3, tokens: '100' })
+  })
 })
