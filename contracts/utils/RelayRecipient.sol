25b8289e5d5ef42ce9866cc55c12cdbeca4549d7
Brendan Asselstine <mail.asselstine@gmail.com>
2020-10-05 12:30:41 -0700

    Updated GSN to v2.0.1

--- contracts/utils/RelayRecipient.sol
+++ contracts/utils/RelayRecipient.sol
@@ -6,10 +6,6 @@ import "@opengsn/gsn/contracts/BaseRelayRecipient.sol";
 
 contract RelayRecipient is BaseRelayRecipient {
   function versionRecipient() external override view returns (string memory) {
-    return "2.0.0-beta.1+pooltogether.relay.recipient";
-  }
-
-  function getTrustedForwarder() external view returns (address) {
-    return trustedForwarder;
+    return "2.0.0";
   }
 }
