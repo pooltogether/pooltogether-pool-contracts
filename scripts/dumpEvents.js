d672048bee81e64ec4f9b0a24edf3057bd1a7b3a
Brendan Asselstine <mail.asselstine@gmail.com>
2020-11-27 15:29:07 -0800

    Added dumpEvents script

---
+++ scripts/dumpEvents.js
@@ -0,0 +1,30 @@
+const buidler = require('@nomiclabs/buidler')
+const fs = require('fs')
+
+async function run() {
+  const { deployments, ethers } = buidler
+  const { provider } = ethers
+
+  const signers = await ethers.getSigners()
+
+  const ticket = await ethers.getContractAt('Ticket', '0x334cBb5858417Aee161B53Ee0D5349cCF54514CF', signers[0])
+
+  const filter = ticket.filters.Transfer()
+
+  filter.fromBlock = 11104392
+
+
+  const logs = await provider.getLogs(filter)
+
+  console.log(`Found ${logs.length} logs`)
+
+  const events = logs.map(log => ticket.interface.parseLog(log))
+
+  const outputFile = fs.openSync('./transfers.csv', 'w')
+  events.forEach(event => {
+    fs.writeSync(outputFile, `${event.args.from},${event.args.to},${event.args.value}\n`)
+  })
+  fs.closeSync(outputFile)
+}
+
+run()
