a404145137fac1158f73b9789d16687555cff17c
Aodhgan <aodhgan@pooltogether.com>
2021-02-10 10:29:30 -0800

    removed console log

--- test/BalanceDripExposed.test.js
+++ test/BalanceDripExposed.test.js
@@ -20,7 +20,6 @@ describe('BalanceDripExposed', function() {
   describe('drip()', () => {
 
     it('should handle being initialized', async () => {
-      console.log('first test')
       await expect(
         dripExposed.drip(
           toWei('0'), // total supply of tokens
