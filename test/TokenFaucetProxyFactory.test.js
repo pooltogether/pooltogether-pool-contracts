743286bd1599d6e781deff67a1c0848802d45c06
Brendan Asselstine <mail.asselstine@gmail.com>
2021-03-02 10:58:53 -0800

    TokenFaucetProxyFactory can create a faucet and deposit atomically

--- test/TokenFaucetProxyFactory.test.js
+++ test/TokenFaucetProxyFactory.test.js
@@ -1,9 +1,9 @@
 const { expect } = require("chai");
-
-
 const hardhat = require('hardhat')
 const {  deployMockContract } = require('ethereum-waffle')
 const { deployments } = require("hardhat")
+
+const toWei = hardhat.ethers.utils.parseEther
 
 let overrides = { gasLimit: 9500000 }
 
@@ -19,10 +19,10 @@ describe('TokenFaucetProxyFactory', () => {
     [wallet, wallet2] = await hardhat.ethers.getSigners()
     provider = hardhat.ethers.provider
 
-    const ERC20MintableContract =  await hre.ethers.getContractFactory("ERC20Mintable", wallet, overrides)
+    const ERC20MintableContract =  await hardhat.ethers.getContractFactory("ERC20Mintable", wallet, overrides)
 
     measure = await ERC20MintableContract.deploy('Measure', 'MEAS')
-    asset = await ERC20MintableContract.deploy('Measure', 'MEAS')
+    asset = await ERC20MintableContract.deploy('Asset', 'ASS')
 
     await deployments.fixture()
     let tokenFaucetProxyFactoryResult = await deployments.get("TokenFaucetProxyFactory")
@@ -30,8 +30,8 @@ describe('TokenFaucetProxyFactory', () => {
   })
 
   describe('create()', () => {
-    it('should create a new comptroller', async () => {
-      let tx = await tokenFaucetProxyFactory.create(asset.address, measure.address, ethers.utils.parseEther('0.01'), overrides)
+    it('should create a new faucet', async () => {
+      let tx = await tokenFaucetProxyFactory.create(asset.address, measure.address, toWei('0.01'), overrides)
       let receipt = await provider.getTransactionReceipt(tx.hash)
       let event = tokenFaucetProxyFactory.interface.parseLog(receipt.logs[0])
       expect(event.name).to.equal('ProxyCreated')
@@ -40,19 +40,33 @@ describe('TokenFaucetProxyFactory', () => {
 
       expect(await tokenFaucet.asset()).to.equal(asset.address)
       expect(await tokenFaucet.measure()).to.equal(measure.address)
-      expect(await tokenFaucet.dripRatePerSecond()).to.equal(ethers.utils.parseEther('0.01'))
+      expect(await tokenFaucet.dripRatePerSecond()).to.equal(toWei('0.01'))
       expect(await tokenFaucet.owner()).to.equal(wallet.address)
-      
+    })
+  })
+
+  describe('createAndDeposit()', () => {
+    it('should create a new faucet and immediately deposit into it', async () => {
+      await asset.mint(wallet.address, toWei('100'))
+      await asset.approve(tokenFaucetProxyFactory.address, toWei('100'))
+
+      let tx = await tokenFaucetProxyFactory.createAndDeposit(asset.address, measure.address, toWei('0.01'), toWei('100'), overrides)
+      let receipt = await provider.getTransactionReceipt(tx.hash)
+      let event = tokenFaucetProxyFactory.interface.parseLog(receipt.logs[0])
+      expect(event.name).to.equal('ProxyCreated')
+      let tokenFaucet = await hardhat.ethers.getContractAt("TokenFaucet", event.args.proxy, wallet)
+
+      expect(await asset.balanceOf(tokenFaucet.address)).to.equal(toWei('100'))
     })
   })
 
   describe('claimAll', () => {
-    it('should call claim on comptrollers', async () => {
-      const TokenFaucet = await hre.artifacts.readArtifact("TokenFaucet")
-      let comptroller = await deployMockContract(wallet, TokenFaucet.abi, overrides)
-      await comptroller.mock.claim.withArgs(wallet.address).revertsWithReason("it was called!")
+    it('should call claim on faucets', async () => {
+      const TokenFaucet = await hardhat.artifacts.readArtifact("TokenFaucet")
+      let faucet = await deployMockContract(wallet, TokenFaucet.abi, overrides)
+      await faucet.mock.claim.withArgs(wallet.address).revertsWithReason("it was called!")
 
-      await expect(tokenFaucetProxyFactory.claimAll(wallet.address, [comptroller.address]))
+      await expect(tokenFaucetProxyFactory.claimAll(wallet.address, [faucet.address]))
         .to.be.revertedWith("it was called!")
     })
   })
