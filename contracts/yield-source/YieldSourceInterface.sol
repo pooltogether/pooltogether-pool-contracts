24bcaf3d9d8f114b9662b9001a28e381ed74e6e8
Brendan Asselstine <mail.asselstine@gmail.com>
2021-03-10 14:02:45 -0800

    Clarified yield source natspec

--- contracts/yield-source/YieldSourceInterface.sol
+++ contracts/yield-source/YieldSourceInterface.sol
@@ -15,12 +15,13 @@ interface YieldSourceInterface {
   /// @return The underlying balance of asset tokens
   function balanceOf(address addr) external returns (uint256);
 
-  /// @notice Supplies asset tokens to the yield source.
-  /// @param mintAmount The amount of asset tokens to be supplied
-  function supplyTo(uint256 mintAmount, address to) external;
+  /// @notice Supplies tokens to the yield source.  Allows assets to be supplied on other user's behalf using the `to` param.
+  /// @param amount The amount of `token()` to be supplied
+  /// @param to The user whose balance will receive the tokens
+  function supplyTo(uint256 amount, address to) external;
 
-  /// @notice Redeems asset tokens from the yield source.
-  /// @param redeemAmount The amount of yield-bearing tokens to be redeemed
+  /// @notice Redeems tokens from the yield source.
+  /// @param amount The amount of `token()` to withdraw.  Denominated in `token()` as above.
   /// @return The actual amount of tokens that were redeemed.
-  function redeem(uint256 redeemAmount) external returns (uint256);
+  function redeem(uint256 amount) external returns (uint256);
 }
