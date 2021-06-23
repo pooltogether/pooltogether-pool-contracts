c5daf1e3215f0c33bb5357f60e59138ac49cef3a
Aodhgan <aodhgan@pooltogether.com>
2021-06-23 15:49:44 -0700

    refactor for DRY

--- scripts/verify.js
+++ scripts/verify.js
@@ -16,23 +16,30 @@ const getContract = async (name) => {
   return hardhat.ethers.getContractAt(name, (await deployments.get(name)).address, signers[0])
 }
 
-const verifyAddress = async (address, name) => {
+const verifyAddress = async (address, name, path = "", args = "") => {
+
+  /*
+    needs to be in form:
+    hardhat verify --config <hardhat.config.<NETWORK>.js>
+      --network <network-name> <address>
+      --contract <PATH-TO-CONTRACT:CONTRACT-NAME>
+      <Constructor Args seperated by spaces>
+
+      as per https://www.npmjs.com/package/@nomiclabs/hardhat-etherscan
+  */
+
   const network = hardhat.network.name
-  let config 
-  if(isBinance()){
-    config = '--config hardhat.config.bsc.js'
-  }
-  else if(isPolygon()){
-    console.log("using polygon config")
-    config ='--config hardhat.config.polygon.js'
-  }
-  else {
-    config = ''
-  }
+  const config = getHardhatConfigFile(network)
+  let contractFlag = ""
   
+  if(path != ""){
+    contractFlag = "--contract " + path 
+  }
+
   try {
-    console.log(`running: hardhat ${config} verify --network ${network} ${address}`)
-    await exec(`hardhat ${config} verify --network ${network} ${address}`)
+    const cmd = `hardhat ${config} verify --network ${network} ${address} ${contractFlag} ${args}`
+    info(`running: ${cmd}`)
+    await exec(cmd.trim())
   } catch (e) {
     if (/Contract source code already verified/.test(e.message)) {
       info(`${name} already verified`)
@@ -43,12 +50,12 @@ const verifyAddress = async (address, name) => {
   }
 }
 
-const verifyProxyFactory = async (name) => {
+const verifyProxyFactoryInstance = async (name) => {
   const proxyFactory = await getContract(name)
   const instanceAddress = await proxyFactory.instance()
-  info(`Verifying ${name} instance at ${instanceAddress}...`)
+  info(`Verifying ${name} Instance at ${instanceAddress}...`)
   await verifyAddress(instanceAddress, name)
-  success(`Verified!`)
+  success(`Verified ${name} Instance!`)
 }
 
 function isBinance() {
@@ -61,84 +68,66 @@ function isPolygon() {
   return /polygon/.test(network) || /matic/.test(network)
 }
 
-function etherscanApiKey() {
-  if (isBinance()) {
-    info(`Using bsc scan api key`)
-    return process.env.BSCSCAN_API_KEY
-  } 
+
+function getHardhatConfigFile(network){
+  let config 
+  if(isBinance()){
+    config = '--config hardhat.config.bsc.js'
+  }
   else if(isPolygon()){
-    info(`Using polygonscan api key`)
-    return process.env.POLYGONSCAN_API_KEY
+    config ='--config hardhat.config.polygon.js'
   }
   else {
-    return process.env.ETHERSCAN_API_KEY
+    config = ''
   }
+  return config
 }
 
 
-/*
-needs to be in form: ardhat verify --config hardhat.config.polygon.js --network matic 0x5effa0823e486A5ED1D49d88A1374Fc337e1f9F4 
-  --contract contracts/builders/PoolWithMultipleWinnersBuilder.sol:PoolWithMultipleWinnersBuilder 
-"0x20F29CCaE4c9886964033042c6b79c2C4C816308" "0x41122Ca50202d13c809dfE88F60Da212A1525Ed7" "0x4d1639e4b237BCab6F908A1CEb0995716D5ebE36" 
-"0xaFcEa072BcBad91029A2bA0b37bAC8269dd4f5E6" "0x72Edd573E230C7d68274Bf718A4C6aD82b5d5f90"
-*/
-async function verifyPolygonScan(){
+async function verifyEtherscanClone(){
+  const network = hardhat.network.name
 
+  info(`verifying contracts on Etherscan Clone`)
 
-  info(`verifying contracts on PolygonScan`)
-
-  const filePath = "./deployments/matic/"
-  const config ='--config hardhat.config.polygon.js'
-
+  const filePath = "./deployments/"+network+"/"
+  
   let toplevelContracts = []
 
+  // read deployment JSON files
   fs.readdirSync(filePath).filter((fileName) => {
-    // console.log("found fileName ", fileName)
     if(fileName.includes(".json")){
 
       const contractName = (fileName.substring(0, fileName.length - 5)).trim() // strip .json
       const contractDirPath = (find.fileSync(contractName+".sol", "./contracts"))[0]
       if(!contractDirPath){
         error(`There is no matching contract for ${contractName}. This is likely becuase the deployment contract name is different from the Solidity contract title.
-         Run verification manually. See verifyPolygonScan() for details`)
+         Run verification manually. See verifyEtherscanClone() for details`)
          return
       }
       const deployment = JSON.parse(fs.readFileSync(filePath+fileName, "utf8"))
 
       toplevelContracts.push({
         address: deployment.address,
-        contractName: contractDirPath + ":" + contractName,
+        contractPath: contractDirPath + ":" + contractName,
+        contractName,
         constructorArgs : deployment.args
       })
     }
   })
 
-  info(`${toplevelContracts.length} contracts to verify`)
+  info(`Attempting to verify ${toplevelContracts.length} top level contracts`)
 
   toplevelContracts.forEach(async (contract)=>{
-    // console.log("attempting to verify ", contract)
-
+    
     let args = ""
 
     if(contract.constructorArgs.length > 0){
       contract.constructorArgs.forEach((arg)=>{
-        args = args.concat("\"", arg, "\" ") // format constructor args in correct form
+        args = args.concat("\"", arg, "\" ") // format constructor args in correct form - "arg" "arg"
       })    
     }
     
-    try {
-      // console.log(`running: hardhat ${config} verify --network matic ${contract.address} --contract ${contract.contractName} ${args}`)
-      await exec(`hardhat ${config} verify --network matic ${contract.address} --contract ${contract.contractName} ${args}`)
-    } catch (e) {
-      if (/Contract source code already verified/.test(e.message)) {
-        info(`${contract.contractName} already verified`)
-      } else {
-        error(e.message)
-        console.error(e)
-      }
-    }
-
-
+    await verifyAddress(contract.address, contract.contractName, contract.contractPath, args)
   })
 }
 
@@ -148,13 +137,14 @@ async function run() {
 
   info(`Verifying top-level contracts on network: ${network}`)
 
-  if(network == "matic"){
-    await verifyPolygonScan()
+  if(network == "matic" || network == "bsc"){
+    await verifyEtherscanClone()
   }
   else {  
-    info(`verifying contracts using etherscan-verify plugin`)
+    info(`verifying contracts using native Hardhat verify`)
+    // using hardhat native etherscan verify -- this supports mainnet, rinkeby, kovan etc. 
     const { stdout, stderr } = await exec(
-      `hardhat --network ${network} etherscan-verify --solc-input --api-key ${etherscanApiKey()}`
+      `hardhat --network ${network} etherscan-verify --solc-input --api-key ${process.env.ETHERSCAN_API_KEY}`
     )
     console.log(chalk.yellow(stdout))
     console.log(chalk.red(stderr))
@@ -164,13 +154,13 @@ async function run() {
 
   info(`Verifying proxy factory instances...`)
 
-  await verifyProxyFactory('CompoundPrizePoolProxyFactory')
-  await verifyProxyFactory('ControlledTokenProxyFactory')
-  await verifyProxyFactory('MultipleWinnersProxyFactory')
-  await verifyProxyFactory('StakePrizePoolProxyFactory')
-  await verifyProxyFactory('TicketProxyFactory')
-  await verifyProxyFactory('TokenFaucetProxyFactory')
-  await verifyProxyFactory('YieldSourcePrizePoolProxyFactory')
+  await verifyProxyFactoryInstance('CompoundPrizePoolProxyFactory')
+  await verifyProxyFactoryInstance('ControlledTokenProxyFactory')
+  await verifyProxyFactoryInstance('MultipleWinnersProxyFactory')
+  await verifyProxyFactoryInstance('StakePrizePoolProxyFactory')
+  await verifyProxyFactoryInstance('TicketProxyFactory')
+  await verifyProxyFactoryInstance('TokenFaucetProxyFactory')
+  await verifyProxyFactoryInstance('YieldSourcePrizePoolProxyFactory')
 
   success('Done!')
 }
